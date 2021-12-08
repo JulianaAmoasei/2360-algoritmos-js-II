dab7350ca414a735d55f00026dea5f20838b0782
Juliana Amoasei <juliana.amoasei@gmail.com>
2021-12-08 14:39:44 -0300

    video 3.2

--- aula-3/encontraMenores.js
+++ aula-3/encontraMenores.js
@@ -22,4 +22,21 @@ function trocaLugar(array, de, para) {
   array[de] = elem2
 }
 
-console.log(encontraMenores(listaLivros[2], listaLivros));
+function divideNoPivo(array) {
+  let pivo = array[Math.floor(array.length / 2)];
+  encontraMenores(pivo, array);
+  let valoresMenores = 0;
+
+  for(let analisando = 0; analisando < array.length; analisando++) {
+    let atual = array[analisando];
+    if(atual.preco <= pivo.preco && atual !== pivo) {
+      trocaLugar(array, analisando, valoresMenores)
+      valoresMenores++
+    }
+  }
+
+  return array;
+}
+
+console.log(divideNoPivo(listaLivros));
+// console.log(encontraMenores(listaLivros[2], listaLivros));
